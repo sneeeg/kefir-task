b44f3207ebd4d12fe9e9fba5c0bdf838ecffb255
Tagir Zainutdinov <tagir0074@gmail.com>
2025-01-28 15:42:51 +0400

    feat: catalog filter

---
+++ components/icons/X.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.9783 6.22897L6.97833 18.229" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.97833 6.22897L18.9783 18.229" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
--- components/sections/catalog/list/index.vue
+++ components/sections/catalog/list/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="catalog-list">
-    <SectionsCatalogListItem v-for="item in items"
-                             :guid="item.guid"
-                             :image="item.image"
-                             :name="item.name"
-                             :price="item.price"
-    />
+  <div>
+    <div class="catalog-filter">
+      <button @click="toggleFilter" class="filter-input">
+        {{ selectedFilter }}
+        <IconsChevronDown :class="{ open: isOpenFilter }" />
+      </button>
+      <button @click="resetFilter" v-if="selectedFilter !== 'Фильтр коллекций'">
+        <IconsX />
+      </button>
+      <div class="filter-dropdown" v-show="isOpenFilter">
+        <div class="filter-dropdown-item" v-for="(item, index) in filterItems" :key="index" @click="setFilter(item)">
+          {{ item }}
+        </div>
+      </div>
+    </div>
+    <div class="catalog-list">
+      <SectionsCatalogListItem v-for="item in filteredData"
+                               :guid="item.guid"
+                               :image="item.image"
+                               :name="item.name"
+                               :price="item.price"
+                               v-if="filteredData"
+      />
+      <SectionsCatalogListItem v-for="item in items"
+                               :guid="item.guid"
+                               :image="item.image"
+                               :name="item.name"
+                               :price="item.price"
+                               v-else
+      />
+    </div>
   </div>
 </template>
 
@@ -96,14 +120,84 @@ const items = ref([
     category: "Monkey"
   }
 ])
+const filterItems = ref(['Bean', 'Dragon', 'Penguin', 'Monkey'])
+const isOpenFilter = ref(false)
+const selectedFilter = ref("Фильтр коллекций")
+const filteredData = ref(undefined)
+const toggleFilter = () => {
+  isOpenFilter.value = !isOpenFilter.value
+}
+const setFilter = (value) => {
+  selectedFilter.value = value;
+  isOpenFilter.value = false;
+  filteredData.value = items.value.filter(i => i.category === value);
+}
+const resetFilter = () => {
+  selectedFilter.value = "Фильтр коллекций";
+  isOpenFilter.value = false;
+  filteredData.value = undefined;
+  console.log(filteredData)
+}
 </script>
 
 <style lang="scss">
 .catalog-list {
-  margin-top: 3rem;
+  margin-top: 1rem;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   column-gap: 18px;
   row-gap: 24px;
+}
+.catalog-filter {
+  position: relative;
+  margin-top: 2rem;
+  gap: 8px;
+  display: flex;
+  align-items: center;
+  .filter-input {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    background: #fff;
+    padding: 0.7rem;
+    border: 1px solid #e6e6e6;
+    border-radius: 5px;
+    font-size: 14px;
+    transition: .3s;
+    color: #4b4b4b;
+    &:hover {
+      background: #f0f0f0;
+    }
+    .open {
+      transform: rotate(180deg);
+      transition: .3s;
+    }
+  }
+  .filter-dropdown {
+    position: absolute;
+    top: 48px;
+    left: 0;
+    min-width: 150px;
+    background: #fff;
+    box-shadow: 4px 4px 8px 0 rgba(34, 60, 80, 0.2);
+    border-radius: 5px;
+    z-index: 1;
+    &-item {
+      padding: 1rem;
+      font-size: 14px;
+      cursor: pointer;
+      border-radius: 5px;
+      transition: .3s;
+      &:not(:last-child) {
+        border-bottom: 1px solid #ccc;
+      }
+      &:hover {
+        background: #ebebeb;
+      }
+      &:active {
+        background: #c8c8c8;
+      }
+    }
+  }
 }
 </style>
